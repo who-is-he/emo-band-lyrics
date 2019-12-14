2fe98927f8a2d97fb490ce6b50cc30f770fe0b6d
Mike <monarch.michael.e@gmail.com>
2019-12-14 02:03:13 -0500

    chain fix

--- create_chain.py
+++ create_chain.py
@@ -2,7 +2,7 @@ import codecs, json
 from collections import defaultdict
 
 path = 'dict.txt'
-replace_chars = "”\"()[]\{}\u6969"
+replace_chars = "”\"()[]\{}\u6969\u0647\u0648\u0627\u0644\u05b1\u05b9\u05e0\u05d5\u05d7\u05d6\u05c2\u05e8\u05b5\u05b4\u05d4\u05d0\u05b6\u05b8\u05d3\u05bc\u05dd\u062f\u05dc\u05d9\u062d\u0635\u0645\u0642\u05e9\u05b0\u05c1\u05de\u05b7\u05e2"
 
 with codecs.open(path, 'r', encoding='utf-8', errors='ignore') as f:
     text = f.read()
